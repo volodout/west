61cbe473c62c7ff23564500219686e93c299a419
mmdoneperson <mohmaks220@gmail.com>
2025-03-18 15:52:49 +0500

    complete task 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import SpeedRate from './SpeedRate.js';
 
 // Отвечает является ли карта уткой.
 function isDuck(card) {
-    return card && card.quacks && card.swims;
+    return card instanceof Duck;
 }
 
 // Отвечает является ли карта собакой.
@@ -27,32 +27,39 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
+// Класс Утки
+class Duck extends Card {
+    constructor(name = 'Мирная утка', power = 2) {
+        super(name, power);
+    }
 
+    quacks() {
+        console.log('quack');
+    }
 
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+    swims() {
+        console.log('float: both;');
+    }
 }
 
-
-// Основа для собаки.
-function Dog() {
+// Класс Собаки
+class Dog extends Card {
+    constructor(name = 'Пес-бандит', power = 3) {
+        super(name, power);
+    }
 }
-
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
-
 
 // Создание игры.
 const game = new Game(seriffStartDeck, banditStartDeck);
